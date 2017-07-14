90f28f35a7d6413de916c9b6ace7aa55c4bf013e
eusekerci <enesugursekerci@gmail.com>
2017-07-14 18:56:13 +0300

    visual assets and scaling

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private SpriteRenderer _renderer;
+
+	void Start ()
+	{
+	    _rigidbody = GetComponent<Rigidbody2D>();
+	    _renderer = GetComponent<SpriteRenderer>();
+
+        _rigidbody.velocity = new Vector2(3, 5);
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
